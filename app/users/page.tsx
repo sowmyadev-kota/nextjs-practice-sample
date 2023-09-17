e37339ad8c393e3ef1b72a45615bfd7fca4a8fa5
Sowmya Kota <kvsowmyasri21@gmail.com>
2023-09-16 22:15:56 -0700

    feat: data fetching on server component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-const UsersPage = () => {
-  return <div>page</div>;
+interface User {
+  id: number;
+  title: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users/1/albums"
+  );
+  const users = await res.json();
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user: User) => (
+          <li key={user.id}>{user.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default UsersPage;
