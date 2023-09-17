86f8c92d4df5cfcc997563001e7dc19e9118df46
Sowmya Kota <kvsowmyasri21@gmail.com>
2023-09-16 21:41:26 -0700

    feat: added server and client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "./users/components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
       <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
   );
 }
